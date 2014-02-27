fa1b4f1be141881ae6466dda7d865f32b173f168
Toon Verwerft <toon.verwerft@phpro.be>
2014-02-27 15:29:33 +0100

    Fix service manager intries

--- config/module.config.php
+++ config/module.config.php
@@ -2,7 +2,9 @@
 return array(
     'service_manager' => array(
       'invokables' => array(
+        'DoctrineHydrationModule\Strategy\ODM\MongoDB\EmbeddedCollection' => 'Phpro\DoctrineHydrationModule\Hydrator\ODM\MongoDB\Strategy\EmbeddedCollection',
         'DoctrineHydrationModule\Strategy\ODM\MongoDB\EmbeddedField' => 'Phpro\DoctrineHydrationModule\Hydrator\ODM\MongoDB\Strategy\EmbeddedField',
+        'DoctrineHydrationModule\Strategy\ODM\MongoDB\ReferencedCollection' => 'Phpro\DoctrineHydrationModule\Hydrator\ODM\MongoDB\Strategy\ReferencedCollection',
         'DoctrineHydrationModule\Strategy\ODM\MongoDB\ReferencedField' => 'Phpro\DoctrineHydrationModule\Hydrator\ODM\MongoDB\Strategy\ReferencedField',
       ),
     ),

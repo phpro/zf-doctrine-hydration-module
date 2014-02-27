a61fc980a9b2bdcaa5b25561d70b1dcccf50ac0c
Toon Verwerft <toon.verwerft@phpro.be>
2014-02-27 14:08:50 +0100

    Change location of services

--- config/module.config.php
+++ config/module.config.php
@@ -2,8 +2,8 @@
 return array(
     'service_manager' => array(
       'invokables' => array(
-        'DoctrineHydrationModule\Strategy\ODM\MongoDB\EmbeddedField' => 'Phpro\DoctrineHydrationModule\Hydrator\Strategy\ODM\MongoDB\EmbeddedField',
-        'DoctrineHydrationModule\Strategy\ODM\MongoDB\ReferencedField' => 'Phpro\DoctrineHydrationModule\Hydrator\Strategy\ODM\MongoDB\ReferencedField',
+        'DoctrineHydrationModule\Strategy\ODM\MongoDB\EmbeddedField' => 'Phpro\DoctrineHydrationModule\Hydrator\ODM\MongoDB\Strategy\EmbeddedField',
+        'DoctrineHydrationModule\Strategy\ODM\MongoDB\ReferencedField' => 'Phpro\DoctrineHydrationModule\Hydrator\ODM\MongoDB\Strategy\ReferencedField',
       ),
     ),
     'hydrators' => array(

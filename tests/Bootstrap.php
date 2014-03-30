1420f75a625b3711b272b0daaba22f23e4da3533
Toon Verwerft <toon.verwerft@phpro.be>
2014-03-30 16:15:39 +0200

    Add basic phpunit setup + test for module class

---
+++ tests/Bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Set error reporting
+ */
+error_reporting( E_ALL | E_STRICT );
+
+/*
+ * Autoloading
+ */
+if (!file_exists($file = __DIR__ . '/../vendor/autoload.php')) {
+    throw new RuntimeException('Install dependencies to run test suite.');
+}
+$loader = require $file;
+$loader->add('Phpro\DoctrineHydrationModule\Tests', __DIR__);
---
+++ tests/Phpro/DoctrineHydrationModule/Tests/ModuleTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Phpro\DoctrineHydrationModule\Tests;
+use Phpro\DoctrineHydrationModule\Module;
+
+/**
+ * Class ModuleTest
+ *
+ * @package tests\Phpro\DoctrineHydrationModule
+ */
+class ModuleTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function it_should_be_initializable()
+    {
+        $module = new Module();
+        $this->assertInstanceOf('Phpro\DoctrineHydrationModule\Module', $module);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_provide_autoloader_configuration()
+    {
+        $module = new Module();
+        $this->assertInstanceOf('Zend\ModuleManager\Feature\AutoloaderProviderInterface', $module);
+        $this->assertInternalType('array', $module->getAutoloaderConfig());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_provide_configuration()
+    {
+        $module = new Module();
+        $this->assertInstanceOf('Zend\ModuleManager\Feature\ConfigProviderInterface', $module);
+        $this->assertInternalType('array', $module->getConfig());
+    }
+}

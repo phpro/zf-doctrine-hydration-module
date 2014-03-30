2ebf218b1ac4b1aceb40c63ce0fce69f66dc9920
Toon Verwerft <toon.verwerft@phpro.be>
2014-03-30 16:18:51 +0200

    Add basic phpunit setup + test for module class

---
+++ tests/Phpro/DoctrineHydrationModule/Tests/Service/DoctrineHydratorFactoryTest.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Phpro ZF2 Library
+     *
+     * @link      http://fisheye.phpro.be/git/Git-Vlir-Uos.git
+     * @copyright Copyright (c) 2012 PHPro
+     * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+     *
+     */
+
+namespace Phpro\DoctrineHydrationModule\Tests\Service;
+
+
+class DoctrineHydratorFactoryTest extends \PHPUnit_Framework_TestCase {
+
+}
+ 

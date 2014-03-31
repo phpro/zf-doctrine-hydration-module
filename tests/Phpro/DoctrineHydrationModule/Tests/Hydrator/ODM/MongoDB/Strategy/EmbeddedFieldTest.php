70bbcdd4d2ac7ff767a4797b3adf0b7bc064924d
Toon Verwerft <toon.verwerft@phpro.be>
2014-03-31 12:44:26 +0200

    Add test for embedded fields strategy

--- tests/Phpro/DoctrineHydrationModule/Tests/Hydrator/ODM/MongoDB/Strategy/EmbeddedFieldTest.php
+++ tests/Phpro/DoctrineHydrationModule/Tests/Hydrator/ODM/MongoDB/Strategy/EmbeddedFieldTest.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace Phpro\DoctrineHydrationModule\Tests\Hydrator\ODM\MongoDB\Strategy;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Phpro\DoctrineHydrationModule\Hydrator\ODM\MongoDB\Strategy\EmbeddedField;
+use Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationEmbedOne;
+use Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationUser;
 use Zend\Stdlib\Hydrator\Strategy\StrategyInterface;
 
 
@@ -18,5 +21,51 @@ class EmbeddedFieldTest extends AbstractMongoStrategyTest
     protected function createStrategy()
     {
         return new EmbeddedField();
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_extract_embedded_fields()
+    {
+        $user = new HydrationUser();
+        $user->setId(1);
+        $user->setName('username');
+
+        $embedded = new HydrationEmbedOne();
+        $embedded->setId(1);
+        $embedded->setName('name');
+        $user->setEmbedOne($embedded);
+
+        /** @var DocumentManager $objectmanager */
+        $objectManager = $this->dm;
+        $metadata = $objectManager->getClassMetadata(get_class($user));
+        $strategy = $this->getStrategy($objectManager, $user, $metadata, 'embedOne');
+
+        $result = $strategy->extract($user->getEmbedOne());
+        $this->assertEquals('name', $result['name']);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_hydrate_embedded_fields()
+    {
+        $user = new HydrationUser();
+        $user->setId(1);
+        $user->setName('username');
+
+        $data = [
+            'id' => 1,
+            'name' => 'name'
+        ];
+
+        /** @var DocumentManager $objectmanager */
+        $objectManager = $this->dm;
+        $metadata = $objectManager->getClassMetadata(get_class($user));
+        $strategy = $this->getStrategy($objectManager, $user, $metadata, 'embedOne');
+
+        $result = $strategy->hydrate($data);
+        $this->assertEquals('name', $result->getName());
     }
 }

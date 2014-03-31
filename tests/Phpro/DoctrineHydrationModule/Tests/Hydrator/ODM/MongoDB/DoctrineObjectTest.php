22209f59deeeb678df5ed6095a784e2ff72d2ae9
Toon Verwerft <toon.verwerft@phpro.be>
2014-03-31 13:09:41 +0200

    Add main hydrator tests

--- tests/Phpro/DoctrineHydrationModule/Tests/Hydrator/ODM/MongoDB/DoctrineObjectTest.php
+++ tests/Phpro/DoctrineHydrationModule/Tests/Hydrator/ODM/MongoDB/DoctrineObjectTest.php
@@ -1,14 +1,20 @@
 <?php
 
 namespace Phpro\DoctrineHydrationModule\Tests\Hydrator\ODM\MongoDB;
+use Doctrine\ODM\MongoDB\Tests\BaseTest;
 use Phpro\DoctrineHydrationModule\Hydrator\ODM\MongoDB\DoctrineObject;
+use Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationEmbedMany;
+use Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationEmbedOne;
+use Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationReferenceMany;
+use Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationReferenceOne;
+use Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationUser;
 
 /**
  * Class DoctrineObjectTest
  *
  * @package Phpro\DoctrineHydrationModule\Tests\Hydrator\ODM\MongoDB
  */
-class DoctrineObjectTest extends \PHPUnit_Framework_TestCase
+class DoctrineObjectTest extends BaseTest
 {
 
     /**
@@ -39,6 +45,118 @@ class DoctrineObjectTest extends \PHPUnit_Framework_TestCase
     {
         $hydrator = $this->createHydrator();
         $this->assertInstanceOf('DoctrineModule\Stdlib\Hydrator\DoctrineObject', $hydrator);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_extract_a_document()
+    {
+        $user = new HydrationUser();
+        $user->setId(1);
+        $user->setName('user');
+
+        $embedOne = new HydrationEmbedOne();
+        $embedOne->setId(1);
+        $embedOne->setName('name');
+        $user->setEmbedOne($embedOne);
+
+        $embedMany = new HydrationEmbedMany();
+        $embedMany->setId(1);
+        $embedMany->setName('name');
+        $user->addEmbedMany([$embedMany]);
+
+        $referenceOne = new HydrationReferenceOne();
+        $referenceOne->setId(1);
+        $referenceOne->setName('name');
+        $user->setReferenceOne($referenceOne);
+
+        $referenceMany = new HydrationEmbedMany();
+        $referenceMany->setId(1);
+        $referenceMany->setName('name');
+        $user->addReferenceMany([$referenceMany]);
+
+        $hydrator = new DoctrineObject($this->dm);
+        $result = $hydrator->extract($user);
+
+        $this->assertEquals(1, $result['id']);
+        $this->assertEquals('user', $result['name']);
+        $this->assertEquals(1, $result['embedOne']['id']);
+        $this->assertEquals('name', $result['embedOne']['name']);
+        $this->assertEquals(1, $result['embedMany'][0]['id']);
+        $this->assertEquals('name', $result['embedMany'][0]['name']);
+        $this->assertEquals(1, $result['referenceOne']);
+        $this->assertEquals(1, $result['referenceMany'][0]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_hydrate_a_document()
+    {
+        $user = new HydrationUser();
+        $data = [
+            'id' => 1,
+            'name' => 'user',
+            'referenceOne' => $this->createReferenceOne('name'),
+            'referenceMany' => [$this->createReferenceMany('name')],
+            'embedOne' => [
+                'id' => 1,
+                'name' => 'name'
+            ],
+            'embedMany' => [
+                [
+                    'id' => 1,
+                    'name' => 'name'
+                ]
+            ]
+        ];
+
+        $hydrator = new DoctrineObject($this->dm);
+        $hydrator->hydrate($data, $user);
+
+        $this->assertEquals(1, $user->getId());
+        $this->assertEquals('user', $user->getName());
+        $this->assertInstanceOf('Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationReferenceOne', $user->getReferenceOne());
+        $this->assertInstanceOf('Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationReferenceMany', $user->getReferenceMany()[0]);
+        $this->assertInstanceOf('Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationEmbedOne', $user->getEmbedOne());
+        $this->assertInstanceOf('Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationEmbedMany', $user->getEmbedMany()[0]);
+        $this->assertEquals('name', $user->getReferenceOne()->getName());
+        $this->assertEquals('name', $user->getReferenceMany()[0]->getName());
+        $this->assertEquals('name', $user->getEmbedOne()->getName());
+        $this->assertEquals('name', $user->getEmbedMany()[0]->getName());
+    }
+
+    /**
+     * @param $name
+     *
+     * @return string
+     */
+    protected function createReferenceOne($name)
+    {
+        $embedded = new HydrationReferenceOne();
+        $embedded->setName($name);
+
+        $this->dm->persist($embedded);
+        $this->dm->flush();
+
+        return $embedded->getId();
+    }
+
+    /**
+     * @param $name
+     *
+     * @return string
+     */
+    protected function createReferenceMany($name)
+    {
+        $embedded = new HydrationReferenceMany();
+        $embedded->setName($name);
+
+        $this->dm->persist($embedded);
+        $this->dm->flush();
+
+        return $embedded->getId();
     }
 
 }

36a227e247765da6c59cfab663e4dee8425a40a7
Toon Verwerft <toon.verwerft@phpro.be>
2014-03-31 12:31:36 +0200

    Test references collections

--- tests/Phpro/DoctrineHydrationModule/Tests/Hydrator/ODM/MongoDB/Strategy/ReferencedCollectionTest.php
+++ tests/Phpro/DoctrineHydrationModule/Tests/Hydrator/ODM/MongoDB/Strategy/ReferencedCollectionTest.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace Phpro\DoctrineHydrationModule\Tests\Hydrator\ODM\MongoDB\Strategy;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Phpro\DoctrineHydrationModule\Hydrator\ODM\MongoDB\Strategy\ReferencedCollection;
+use Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationReferenceMany;
+use Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationUser;
 use Zend\Stdlib\Hydrator\Strategy\StrategyInterface;
 
 
@@ -18,5 +21,82 @@ class ReferencedCollectionTest extends AbstractMongoStrategyTest
     protected function createStrategy()
     {
         return new ReferencedCollection();
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_extract_referenced_collections()
+    {
+        $user = new HydrationUser();
+        $user->setId(1);
+        $user->setName('username');
+
+        $referenced = new HydrationReferenceMany();
+        $referenced->setId(1);
+        $referenced->setName('name');
+        $user->addReferenceMany([$referenced]);
+
+        /** @var DocumentManager $objectmanager */
+        $objectManager = $this->dm;
+        $metadata = $objectManager->getClassMetadata(get_class($user));
+
+        $strategy = $this->createStrategy();
+        $strategy->setObject($user);
+        $strategy->setObjectManager($objectManager);
+        $strategy->setCollectionName('referenceMany');
+        $strategy->setClassMetadata($metadata);
+
+        $result = $strategy->extract($user->getReferenceMany());
+        $this->assertEquals(1, $result[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_hydrate_referenced_collections()
+    {
+        $user = new HydrationUser();
+        $user->setId(1);
+        $user->setName('username');
+
+        $id = $this->createReference('name');
+        $data = [
+            [
+                'id' => $id,
+                'name' => 'name'
+            ]
+        ];
+
+        /** @var DocumentManager $objectmanager */
+        $objectManager = $this->dm;
+        $metadata = $objectManager->getClassMetadata(get_class($user));
+
+        $strategy = $this->createStrategy();
+        $strategy->setObject($user);
+        $strategy->setObjectManager($objectManager);
+        $strategy->setCollectionName('referenceMany');
+        $strategy->setClassMetadata($metadata);
+
+        $strategy->hydrate($data);
+        $this->assertEquals('name', $user->getReferenceMany()[0]->getName());
+    }
+
+    /**
+     * Create a reference in the database:
+     *
+     * @param $name
+     *
+     * @return string
+     */
+    protected function createReference($name)
+    {
+        $embedded = new HydrationReferenceMany();
+        $embedded->setName($name);
+
+        $this->dm->persist($embedded);
+        $this->dm->flush();
+
+        return $embedded->getId();
     }
 }

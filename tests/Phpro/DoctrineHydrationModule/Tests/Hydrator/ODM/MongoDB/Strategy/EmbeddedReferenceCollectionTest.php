eda6edb622db35ad34ead37ce0936000264c9784
Toon Verwerft <toon.verwerft@phpro.be>
2014-03-31 12:53:02 +0200

    Add embedded reference collections test

--- tests/Phpro/DoctrineHydrationModule/Tests/Hydrator/ODM/MongoDB/Strategy/EmbeddedReferenceCollectionTest.php
+++ tests/Phpro/DoctrineHydrationModule/Tests/Hydrator/ODM/MongoDB/Strategy/EmbeddedReferenceCollectionTest.php
@@ -2,6 +2,8 @@
 
 namespace Phpro\DoctrineHydrationModule\Tests\Hydrator\ODM\MongoDB\Strategy;
 use Phpro\DoctrineHydrationModule\Hydrator\ODM\MongoDB\Strategy\EmbeddedReferenceCollection;
+use Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationReferenceMany;
+use Phpro\DoctrineHydrationModule\Tests\Fixtures\ODM\MongoDb\HydrationUser;
 use Zend\Stdlib\Hydrator\Strategy\StrategyInterface;
 
 
@@ -19,6 +21,60 @@ class EmbeddedReferenceCollectionTest extends AbstractMongoStrategyTest
     protected function createStrategy()
     {
         return new EmbeddedReferenceCollection();
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_extract_embedded_collections()
+    {
+        $user = new HydrationUser();
+        $user->setId(1);
+        $user->setName('username');
+
+        $referenced = new HydrationReferenceMany();
+        $referenced->setId(1);
+        $referenced->setName('name');
+        $user->addReferenceMany([$referenced]);
+
+        $strategy = $this->getStrategy($this->dm, $user, 'referenceMany');
+        $result = $strategy->extract($user->getReferenceMany());
+        $this->assertEquals('name', $result[0]['name']);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_hydrate_referenced_collections()
+    {
+        $user = new HydrationUser();
+        $user->setId(1);
+        $user->setName('username');
+
+        $id = $this->createReference('name');
+        $data = [$id];
+
+        $strategy = $this->getStrategy($this->dm, $user, 'referenceMany');
+        $strategy->hydrate($data);
+        $this->assertEquals('name', $user->getReferenceMany()[0]->getName());
+    }
+
+    /**
+     * Create a reference in the database:
+     *
+     * @param $name
+     *
+     * @return string
+     */
+    protected function createReference($name)
+    {
+        $embedded = new HydrationReferenceMany();
+        $embedded->setName($name);
+
+        $this->dm->persist($embedded);
+        $this->dm->flush();
+
+        return $embedded->getId();
     }
 
 }

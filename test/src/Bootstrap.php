c6af157eccb8c24aeb1c2655dffa669e559345cd
Tom Anderson <tom.h.anderson@gmail.com>
2014-05-21 23:39:52 -0700

    Reordered dirs to trim the fat PSR-4 allows.

--- test/src/Bootstrap.php
+++ test/src/Bootstrap.php
@@ -4,8 +4,8 @@
 namespace PhproTest\DoctrineHydrationModule;
 
 error_reporting( E_ALL | E_STRICT );
-define('PROJECT_BASE_PATH', __DIR__ . '/../../..');
-define('TEST_BASE_PATH', __DIR__ . '/../..');
+define('PROJECT_BASE_PATH', __DIR__ . '/../..');
+define('TEST_BASE_PATH', __DIR__ . '/..');
 
 $autoloadFile = PROJECT_BASE_PATH . '/vendor/autoload.php';
 if (!file_exists($autoloadFile)) {
